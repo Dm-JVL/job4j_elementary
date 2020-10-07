b7b551ee62a85b16bf714267de4b75ffc9dfbba1
Dm-JVL <JVLEARN@YANDEX.RU>
2020-10-07 22:37:25 +0300

    5.7. Ипотека [#374068]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        double debt = amount;
+        while (debt > 0) {
+            year++;
+            debt = debt * (1 + percent / 100) - salary;
+
+        }
+
+        return year;
+    }
+}
