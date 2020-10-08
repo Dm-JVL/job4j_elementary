3c3d3844ac9e735b3c6d0ce119143c50232a6500
Dm-JVL <JVLEARN@YANDEX.RU>
2020-10-08 18:22:35 +0300

    6.0.3.1. Массивы и цикл for-earch [#374001]

--- src/ru/job4j/array/ArrayLoop.java
+++ src/ru/job4j/array/ArrayLoop.java
@@ -2,12 +2,17 @@ package ru.job4j.array;
 
 public class ArrayLoop {
     public static void main(String[] args) {
+        System.out.println("\nЗадача для п. 6.0.3. Массивы и цикл for. [#373992]:");
         int[] number = new int[5];
         for (int i = 0; i < 5; i++) {
             number[i] = i * 2 + 3;
         }
         for (int i = 0; i < 5; i++) {
             System.out.println(number[i]);
+        }
+        System.out.println("\nЗадача для п. 6.0.3.1. Массивы и цикл for-earch [#374001]:");
+        for (int num: number) {
+            System.out.println(num);
         }
     }
 }
