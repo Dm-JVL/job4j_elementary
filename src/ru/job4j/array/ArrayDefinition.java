c756e770b7782cffd1a12d98d0991e185ff3c534
Dm-JVL <JVLEARN@YANDEX.RU>
2020-10-08 13:47:22 +0300

    6.0.1.1. Размер массива [#374002]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -7,5 +7,20 @@ public class ArrayDefinition {
         String[] surname = new String[100500];
         float[] prices = new float[40];
 
+        System.out.println("SHORT - " + ages.length);
+        System.out.println("STRING - " + surname.length);
+        System.out.println("PRICES - " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Dmitriy";
+        names[1] = "Ilya";
+        names[2] = "Egor";
+        names[3] = "Mariya";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+    }
     }
 }
