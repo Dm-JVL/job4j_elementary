9dd21510f6ea9792c24b8f7ee239daf4deb44201
Dm-JVL <JVLEARN@YANDEX.RU>
2020-10-13 00:04:57 +0300

    6.7.2. Моно столбец в матрице. [#373989] 6.7.3. Массив из диагонали матрицы. [#373990]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -11,4 +11,23 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][column] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for (int index = 0; index < board.length; index++) {
+            rsl[index] = board[index][index];
+        }
+        return rsl;
+    }
 }
