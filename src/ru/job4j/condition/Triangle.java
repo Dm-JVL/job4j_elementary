4e1eedf7a6d3a1b6164bb93ada7be56ca8adf4c8
Dm-JVL <JVLEARN@YANDEX.RU>
2020-11-25 18:59:21 +0300

    2.2. Рефакторинг - Площадь треугольника. [#374166]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,8 +1,39 @@
 package ru.job4j.condition;
 
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
 
     public static boolean exist(double ab, double ac, double bc) {
         return ((ab + ac > bc) && (ac + bc > ab) && (ab + bc > ac));
     }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distanceNew(second);
+        double ac = first.distanceNew(third);
+        double bc = second.distanceNew(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+
+
 }
--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -2,6 +2,7 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
@@ -9,5 +10,15 @@ public class TriangleTest {
     public void whenExist() {
         boolean result = Triangle.exist(2.0, 2.0, 2.0);
         assertThat(result, is(true));
+    }
+
+    @Test
+    public void period() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(8, 0.001));
     }
 }
