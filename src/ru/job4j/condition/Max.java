f8c433cc2c037291b8dd99b87cd93d38620ed0c0
Dm-JVL <JVLEARN@YANDEX.RU>
2020-11-26 10:14:42 +0300

    3. Перегрузить метод max для трех чисел [#374168]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -6,6 +6,14 @@ public class Max {
         return result;
     }
 
+    public static int max(int first, int second, int third) {
+        return (max(max(first, second), third));
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return (max(max(max(first, second), third), fourth));
+    }
+
     public static void main(String[] args) {
         System.out.println("Максимум 1 и 2 = " + Max.max(1, 2));
         System.out.println("Максимум 4 и 1 = " + Max.max(4, 1));
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -21,5 +22,23 @@ public class MaxTest {
     public void whenMax4To4Then4() {
         int result = Max.max(4, 4);
         assertThat(result, is(4));
+    }
+
+    @Test
+    public void when3Max2To4Then4() {
+        int result = Max.max(4, 3, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void when4Max1To4Then4() {
+        int result = Max.max(1, 3, 2, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void when4MaxTo0Then4() {
+        int result = Max.max(0, 0, 0, -1);
+        assertThat(result, is(0));
     }
 }
